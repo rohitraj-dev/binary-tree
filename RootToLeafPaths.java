3234f92f0016fdc5f1a82d4e6b44065a8e45c69e
Rohit Raj <rajrohit7388@outlook.com>
2026-07-28 22:27:13 +0530

    Add files via upload

---
+++ RootToLeafPaths.java
@@ -0,0 +1,26 @@
+import java.util.*;
+class RootToLeafPaths
+{
+    public static ArrayList<ArrayList<Integer>> Paths(Node root)
+    {
+        ArrayList<ArrayList<Integer>> ans = new ArrayList<>();
+        ArrayList<Integer> arr = new ArrayList<>();
+        dfs(root, arr, ans);
+        return ans;
+    }
+    private static void dfs(Node root, ArrayList<Integer> arr, ArrayList<ArrayList<Integer>> ans)
+    {
+        if(root==null)
+            return;
+        arr.add(root.data);
+        if(root.left==null && root.right==null)
+        {
+            ArrayList<Integer> list = new ArrayList<>();
+            list.addAll(arr);
+            ans.add(list);
+        }
+        dfs(root.left, arr,ans);
+        dfs(root.right, arr, ans);
+        arr.remove(arr.size()-1);
+    }
+}
